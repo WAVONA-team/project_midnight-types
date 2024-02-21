282a23815d6aecdddd0f6c2f65ead933d516024d
Jegor volkov <jegor.volkov.a@gmail.com>
2024-02-21 21:33:34 +0100

    feat(TAS-UPDATE-TYPES): update types

--- src/declarations/NormalizedUser.ts
+++ src/declarations/NormalizedUser.ts
@@ -1,7 +1,18 @@
+import { Playlist } from "./Playlist";
+import { Track } from "./Track";
+
 export type NormalizedUser = {
   id: string;
   email: string;
 
   createdAt: Date;
   updatedAt: Date;
+
+  spotifyOAUTH: string | null;
+  yandexOAUTH: string | null;
+  vkOAUTH: string | null;
+  appleOAUTH: string | null;
+
+  tracks?: Track[] | null;
+  playlist?: Playlist[] | null;
 };
