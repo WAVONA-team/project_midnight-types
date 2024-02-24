0858fcc13467a6ea4c9310837cd1388e1e12fa2f
Jegor volkov <jegor.volkov.a@gmail.com>
2024-02-24 20:36:46 +0100

    fix(MASTER): fix tsconfig

--- types/declarations/RefreshToken.d.ts
+++
@@ -1,6 +0,0 @@
-import { type User } from "./User";
-export type Token = {
-    refreshToken: string;
-    userId: string;
-    user?: User | null;
-};
--- types/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { type Token } from "./declarations/RefreshToken";
-export { type User, type NormalizedUser, type UserWithAccessToken, } from "./declarations/User";
-export { type Track } from "./declarations/Track";
-export { type Playlist } from "./declarations/Playlist";
