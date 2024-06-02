dfeaa80a20d2de702817d963e39756fcff76de44
Vo7kov <jegor.volkov.a@gmail.com>
2024-06-02 19:16:23 +0200

    fix(MASTER): fix build

--- types/declarations/Track.d.ts
+++ types/declarations/Track.d.ts
@@ -4,6 +4,7 @@ export type Track = {
     id: string;
     userIdTracks: string;
     userIdSearchHistory: string;
+    isFavourite: boolean;
     createdAt: Date;
     updatedAt: Date;
     title: string;
